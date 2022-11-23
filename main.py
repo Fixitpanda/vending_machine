f61a6bc9e0b613d86e92651aaa6ef3b5a80e62cd
George Khananaev <george.khananaev@gmail.com>
2022-11-23 18:00:15 +0200

    better stracture

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
 import time
-
-# local classes, functions, packages.
 from components.functions import *
-from components.classes import *
 from components import localColors, dataManager
 
 inserted_coins = {}  # empty list for coins processing.
+
+
+# get data by self-oriented index from data with staticmethod.
+class getDrink:
+    # static functions returns parameters by index number, without looping.
+    @staticmethod
+    def name(index_number) -> int:
+        return list(dataManager.drinks.types.keys())[index_number]
+
+    @staticmethod
+    def value(index_number) -> int:
+        return list(dataManager.drinks.types.values())[index_number][0]
+
+    @staticmethod
+    def inventory(index_number) -> int:
+        return list(dataManager.drinks.types.values())[index_number][1]
 
 
 # creating colorful menu based on drinks/stock inside the inventory.json file.
